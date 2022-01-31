0382af5af075a708bdc9c8572473d83a147bfa5f
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-01-31 08:11:04 -0800

    commit tree c5288fcbaedb1981dc92406369b0db08ff3a04d7

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+    get 'cheeses', to: 'cheeses#index'
+
+    resource :cheeses, only: [:show, :create, :new, :update, :edit, :destroy]
+
+
 end
